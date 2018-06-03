9a9f76b1b8d1d7c2beed44d144fd7fd78754823e
mcapel <mateo.capelletti@gmail.com>
2018-06-03 17:52:20 +0300

    removed useless code

--- Colors.hs
+++ Colors.hs
@@ -2,24 +2,15 @@ module Colors where
 import Data.List hiding (partition)
 import Data.Char
 
-weight :: String -> Int
-weight xs = sum [ count x xs | x <- xs ]
-
 count :: Eq a => a -> [a] -> Int
 count x [] = 0
 count x (y:ys) | x==y = 1 + count x ys
                | otherwise = count x ys 
 
-sortedColors :: [String]
-sortedColors = sortBy cmpWeight colors
-
-cmpWeight :: String -> String -> Ordering
-cmpWeight a b = compare (weight a) (weight b)
-
 -- Builds sequences of 4 colors out of 6 possible colors. 
 colors :: [String]
 colors = 
-    mix (chars 6) 6
+    mix (chars 6) 4
     where
       chars' = map chr [97..122] 
       chars i = take i chars'
