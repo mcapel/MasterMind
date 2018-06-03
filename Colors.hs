bfbd00a862aea0f0f8f68be5d01d9f6b6b8f77d9
mcapel <mateo.capelletti@gmail.com>
2018-06-03 18:18:27 +0300

    recovered

--- Colors.hs
+++ Colors.hs
@@ -2,10 +2,19 @@ module Colors where
 import Data.List hiding (partition)
 import Data.Char
 
+weight :: String -> Int
+weight xs = sum [ count x xs | x <- xs ]
+
 count :: Eq a => a -> [a] -> Int
 count x [] = 0
 count x (y:ys) | x==y = 1 + count x ys
                | otherwise = count x ys 
+
+sortedColors :: [String]
+sortedColors = sortBy cmpWeight colors
+
+cmpWeight :: String -> String -> Ordering
+cmpWeight a b = compare (weight a) (weight b)
 
 -- Builds sequences of 4 colors out of 6 possible colors. 
 colors :: [String]
